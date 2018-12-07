8e4e6d0414e02f1a7f2cff7e7e85965cabda7df5
Harel Malka <harel@harelmalka.com>
2018-12-07 13:59:01 +0000

    initial commit. Very WIP

---
+++ examples/index.js
@@ -0,0 +1,113 @@
+import SuggestPlugin from '../lib';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import initialState from './state.json';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+import {SuggestionsContext} from '../lib/context';
+import Tags from './tags.json';
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getCurrentWord(text, index, initialIndex) {
+  if (index === initialIndex) {
+    return { start: getCurrentWord(text, index - 1, initialIndex), end: getCurrentWord(text, index + 1, initialIndex) }
+  }
+  if (text[index] === " " || text[index] === "@" || text[index] === undefined) {
+    return index
+  }
+  if (index < initialIndex) {
+    return getCurrentWord(text, index - 1, initialIndex)
+  }
+  if (index > initialIndex) {
+    return getCurrentWord(text, index + 1, initialIndex)
+  }
+}
+
+const testPI = {
+  onChange: (editor) => {
+    console.log("TESTPI",editor.change())
+  }
+}
+
+class Example extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.suggestPlugin = SuggestPlugin({
+      context: SuggestionsContext,
+      trigger: '@',
+      capture: /@(\S*)$/,
+      options: props.options,
+      onSearch: (editor, query) => {
+        const suggestions = this.search(query);
+        return suggestions;
+      },
+      updateState: this.updateState
+    })
+
+    this.plugins = [
+      testPI,
+      this.suggestPlugin
+    ]
+  }
+
+  updateState = (state, callback) => {
+    this.setState(state, callback);
+  }
+
+  setSuggestions = (results) => {
+    this.setState({
+      results: results
+    })
+  }
+
+  state = {
+    value: Value.fromJSON(initialState),
+    results: Tags,
+    updateSuggestions: this.setSuggestions
+  };
+
+  onChange = ({value}) => {
+    console.log("VALUE", value)
+    this.setState({ value })
+  }
+
+  /**
+   * Perform a search/fetch or filtering of tags based on query value
+   * @param  {string} query
+   */
+  search = (query) => {
+    const suggestions = query ? Tags.filter((item) => item.key.startsWith(query)) : Tags;
+    this.setState({
+      results: suggestions
+    })
+  }
+
+  render = () => {
+    const {value} = this.state;
+    return (
+      <div>
+        <SuggestionsContext.Provider value={this.state}>
+          <Editor
+            spellCheck
+            autoFocus
+            plugins={this.plugins}
+            value={value}
+            onChange={this.onChange}
+          />
+        </SuggestionsContext.Provider>
+      </div>
+    )
+  }
+}
+
+const example = <Example />
+const root = document.body.querySelector('main')
+ReactDOM.render(example, root)
