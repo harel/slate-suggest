28f6294149bb0faf09c471ed1d21da79c609e7d9
Harel Malka <harel@harelmalka.com>
2018-12-16 23:25:56 +0000

    clean up

--- examples/index.js
+++ examples/index.js
@@ -6,110 +6,79 @@ import { Editor } from 'slate-react';
 import { Value } from 'slate';
 import {SuggestionsContext} from '../lib/context';
 import Tags from './tags.json';
-
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getCurrentWord(text, index, initialIndex) {
-  if (index === initialIndex) {
-    return { start: getCurrentWord(text, index - 1, initialIndex), end: getCurrentWord(text, index + 1, initialIndex) }
-  }
-  if (text[index] === " " || text[index] === "@" || text[index] === undefined) {
-    return index
-  }
-  if (index < initialIndex) {
-    return getCurrentWord(text, index - 1, initialIndex)
-  }
-  if (index > initialIndex) {
-    return getCurrentWord(text, index + 1, initialIndex)
-  }
-}
-
-const testPI = {
-  onChange: ({value}) => {
-    console.log("TESTPI",value)
-  }
-}
+ 
 
 class Example extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.lastInputValue = undefined;
-    this.editorRef = React.createRef();
-    this.suggestPlugin = SuggestPlugin({
-      context: SuggestionsContext,
-      trigger: '@',
-      capture: /@(\S*)$/,
-      options: props.options,
-      onSearch: (editor, query) => {
-        const suggestions = this.search(query);
-        return suggestions;
-      },
-      updateState: this.updateState,
-      editorRef: this.editorRef,
-    })
+    constructor(props) {
+        super(props);
+        this.lastInputValue = undefined;
+        this.editorRef = React.createRef();
 
-    this.plugins = [
-      // testPI,
-      this.suggestPlugin
-    ]
-  }
+        // set up the plugin
+        this.suggestPlugin = SuggestPlugin({
+            context: SuggestionsContext,
+            editorRef: this.editorRef,
+            // The symbol or sequence that triggers the suggestion portal
+            triggerSymbol: '@',
+            // The full regex of what qualifies a suggestion lookup
+            triggerRegEx: /@(\S*)$/,
+            // handle requests to search for suggestion values
+            onSearch: (editor, query) => { 
+                return this.search(query);
+            },
+            // handle change events within the plugin, accepting  new state and an optional callback
+            onChange: (state, callback) => {
+                this.setState(state, callback);
+            },
+        })
 
+        this.plugins = [
+            this.suggestPlugin
+        ]
+	}
+  
+    state = {
+        value: Value.fromJSON(initialState),
+        results: Tags, 
+        activeIndex: null,
+    };
 
-  updateState = (state, callback) => {
-    this.setState(state, callback);
-  }
+    onChange = ({value}) => {
+        this.setState({ value })
+    }
 
-  setSuggestions = (results) => {
-    this.setState({
-      results: results
-    })
-  }
+    /**
+     * Perform a search/fetch or filtering of tags based on query value
+     * @param  {string} query
+     */
+    search = (query) => {
+        setTimeout(() => {
+            const suggestions = query ? Tags.filter((item) => item.key.toUpperCase().startsWith(query.toUpperCase())) : Tags;
+            console.log("SUGGESTIONS: ", suggestions)
+            this.setState({
+                results: suggestions
+            })
+        }, 50);
+    }
 
-  state = {
-    value: Value.fromJSON(initialState),
-    results: Tags,
-    updateSuggestions: this.setSuggestions,
-    activeIndex: 1,
-  };
-
-  onChange = ({value}) => {
-    this.setState({ value })
-  }
-
-  /**
-   * Perform a search/fetch or filtering of tags based on query value
-   * @param  {string} query
-   */
-  search = (query) => {
-    const suggestions = query ? Tags.filter((item) => item.key.toUpperCase().startsWith(query.toUpperCase())) : Tags;
-    this.setState({
-      results: suggestions
-    })
-  }
-
-  render = () => {
-    const {value} = this.state;
-    return (
-      <div>
-        <SuggestionsContext.Provider value={this.state}>
-          <Editor
-            spellCheck
-            autoFocus
-            ref={this.editorRef}
-            plugins={this.plugins}
-            value={value}
-            onChange={this.onChange}
-          />
-        </SuggestionsContext.Provider>
-      </div>
-    )
-  }
+    render = () => {
+        const {value} = this.state;
+        return (
+        <div>
+            <SuggestionsContext.Provider value={this.state}>
+            <Editor
+                spellCheck
+                autoFocus
+                ref={this.editorRef}
+                plugins={this.plugins}
+                value={value}
+                onChange={this.onChange}
+            />
+            </SuggestionsContext.Provider>
+        </div>
+        )
+    }
 }
 
 const example = <Example />
